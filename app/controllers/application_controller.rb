100015ba32c7d41bb16162636f6235a3721c81f1
Andrew Lechowicz <andrew@alecho.com>
2015-08-19 19:59:31 -0400

    Setups CORS :boom:

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < JSONAPI::ResourceController
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
+
+  after_action :cors_set_access_control_headers
+
+  def cors_set_access_control_headers
+    headers['Access-Control-Allow-Origin']      = '*'
+    headers['Access-Control-Allow-Methods']     = 'POST, GET, OPTIONS'
+    headers['Access-Control-Max-Age']           = '1728000'
+    headers['Access-Control-Allow-Credentials'] = 'true'
+  end
 end
