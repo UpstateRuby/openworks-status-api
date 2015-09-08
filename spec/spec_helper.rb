d4f6bffcf5f2341f06eed638562e4c14126b39cf
Pete Broderick <simplybinary@gmail.com>
2015-09-08 09:29:28 -0400

    #14 - includes support files in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,8 @@
 require 'factory_girl_rails'
 require 'rails_helper'
 
+Dir[Rails.root.join("spec/**/support/**/*.rb")].each { |f| require f }
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
