3169e0f18fecf8a118fb02a67780120da519b78e
Pete Broderick <simplybinary@gmail.com>
2015-09-16 19:43:47 -0400

    Turning off asset pipeline so I can deploy to heroku

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module OpenworksStatusApi
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-
+    config.assets.enabled = false
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
         origins '*'
