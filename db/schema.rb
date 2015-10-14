38f0aa9d91baaec3167bdab5a3565b5169410fb1
Anthony Lassiter <anthonylassiter@gmail.com>
2015-10-14 19:19:07 -0400

    For some reason, got this commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,12 @@
 
 ActiveRecord::Schema.define(version: 20150921161235) do
 
+  create_table "internets", force: :cascade do |t|
+    t.string   "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "locations", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
