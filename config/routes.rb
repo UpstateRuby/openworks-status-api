c56c52db99adc6d9657be7638f2d085c0449a480
Anthony Lassiter <anthonylassiter@gmail.com>
2016-04-13 19:58:39 -0400

    Changes to use ams

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-	jsonapi_resources :locations
-	jsonapi_resources :things
-	jsonapi_resources :properties
+	# jsonapi_resources :locations
+	# jsonapi_resources :things
+	# jsonapi_resources :properties
+
+  resources :locations
+  resources :things
+  resources :properties
 end
